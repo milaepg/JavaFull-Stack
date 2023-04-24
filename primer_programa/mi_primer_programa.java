f8b58d9d1b777cbec4f1ca3f961b4ee271dc4e3f
Camila Prado <50000125+milaepg@users.noreply.github.com>
2023-04-24 17:08:50 -0400

    primer programa

---
+++ primer_programa/mi_primer_programa.java
@@ -0,0 +1,11 @@
+public class mi_primer_programa
+{    public static void main(String[] args) {
+        String nombre = "Coding Dojo";
+        String edadString = "100";
+        int edad = Integer.parseInt(edadString);
+        String ciudad = "Burbank, CA";
+
+        System.out.println("Mi nombre es " + nombre + ", tengo " + edad + " años de edad.");
+        System.out.println("Mi ciudad es " + ciudad);
+    }
+}
