6e7ef44570cfd8d912fa3cde2eead3751f782f69
Camila Prado <50000125+milaepg@users.noreply.github.com>
2023-05-07 21:43:31 -0400

    Mapa de Hashmatique

---
+++ MapOfTheHashmatique/MapOfTheHasmatiqueTest.java
@@ -0,0 +1,10 @@
+package MapOfTheHashmatique;
+
+public class MapOfTheHasmatiqueTest {
+    public static void main(String[] args){
+        MapOfTheHashmatique bandInfo = new MapOfTheHashmatique();
+    
+        bandInfo.retriveSongData();
+        }
+    
+}
