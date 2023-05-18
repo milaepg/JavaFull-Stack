44faf240cfc0e84dfa7667a6406911f74fa8748a
Camila Prado <50000125+milaepg@users.noreply.github.com>
2023-05-17 23:41:08 -0400

    ninjagold

---
+++ ninjagold/src/main/java/com/camila/ninjagold/controllers/GoldController.java
@@ -0,0 +1,21 @@
+package com.camila.ninjagold.controllers;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoldController {
+	@RequestMapping("/")
+	public String gold(HttpSession session) {
+		if(session.getAttribute("gold") == null) {
+			session.setAttribute("gold", 0);
+			session.setAttribute("activities", new ArrayList<String>());
+		}
+		return "index.jsp";
+	}
+
+}
---
+++ ninjagold/src/main/java/com/camila/ninjagold/controllers/ResetController.java
@@ -0,0 +1,16 @@
+package com.camila.ninjagold.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResetController {
+	@RequestMapping("/reset")
+	public String reset(HttpSession session) {
+		session.removeAttribute("gold");
+		return "redirect:/";
+		}
+
+}
